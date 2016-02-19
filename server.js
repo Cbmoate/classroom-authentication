f4d0493fbcfa9378513261cff6c959f4cb6e59c8
CB <cbmoate@gmail.com>
2016-02-19 11:16:18 -0500

    added package.json and basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('user_info_db', 'root', "");
+var bcrypt = require('bcryptjs');
+var mysql = require('mysql')
+var PORT = process.env.NODE_ENV || 3000;
+var app = express();
